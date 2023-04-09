f47d2264f0e97011d5bf9f416d820ae7f102b4fe
zxchen92 <zixing_92@hotmail.com>
2023-04-09 13:46:07 +0800

    [FYP-15/29]Added frontend and function to view and manage businesses

--- Homepage/admin.py
+++ Homepage/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import FoodCategory, Rating, UserType, UserProfile, BusinessProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff')
+
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(FoodCategory)
 admin.site.register(UserType)
 admin.site.register(UserProfile)
