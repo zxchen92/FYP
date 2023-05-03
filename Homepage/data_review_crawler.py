1e83039444a9080d79195132bb8658f8086ef636
Cheng Yi <chengyi1998@gmail.com>
2023-05-04 00:13:33 +0800

    Add export foodrating excel in data review crawler

--- Homepage/data_review_crawler.py
+++ Homepage/data_review_crawler.py
@@ -116,9 +116,12 @@ def data_review_crawler():
     print(dfTwo)
 
     dfTwo["userid;name"] = dfTwo['userID'] +";"+ dfTwo["name"]
-
+    dfTwo["userid;foodid;rating"] = dfTwo['userID'] +";"+ dfTwo["Food ID"] +";"+ dfTwo["rating"]
     dfTwo.to_excel(r'google_review.xlsx',index=False)
+    
     dfTwo["userid;name"].to_excel(r'UserID.xlsx',index=False)
+    dfTwo["userid;name"].to_excel(r'FoodRatings.xlsx',index=False)
+
 
     messageComplete = "Completed!"
         
